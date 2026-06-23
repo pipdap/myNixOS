06c70a9cbf319c6283cef2825e064e8861de7cf4
pincet <pincet76@gmail.com>
2026-06-23 23:37:44 +0300

    Add niri and noctalia modules, fix permissions

---
+++ modules/features/niri.nix
@@ -0,0 +1,24 @@
+{ self, inputs, ... }: {
+  flake.nixosModules.niri = { pkgs, lib, ... }: {
+    programs.niri = {
+      enable = true;
+      package = self.packages.${pkgs.stdenv.hostPlatform.system}.myNiri;
+    };
+  };
+
+  perSystem = { pkgs, lib, self', ... }: {
+    packages.myNiri = inputs.wrapper-modules.wrappers.niri.wrap {
+      inherit pkgs;
+      settings = {
+        xwayland-satellite.path = lib.getExe pkgs.xwayland-satellite;
+        input.keyboard.xkb.layout = "us,ru";
+        input.keyboard.xkb.options = "grp:alt_shift_toggle";
+        layout.gaps = 5;
+        binds = {
+          "Mod+Return".spawn-sh = lib.getExe pkgs.alacritty;
+          "Mod+Q".close-window = null;
+        };
+      };
+    };
+  };
+}
