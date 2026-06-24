43475fbcd984ddc3db6cee8e51426480a4bc6d0f
pincet <pincet76@gmail.com>
2026-06-24 19:15:38 +0300

    Remove duplicate boot.extraModulePackages

--- modules/hosts/myMachine/hardware.nix
+++ modules/hosts/myMachine/hardware.nix
@@ -1,13 +1,11 @@
 { self, inputs, ... }: {
   flake.nixosModules.myMachineHardware = { config, lib, pkgs, ... }: {
     imports = [ ];
-    
-    boot.extraModulePackages = [ ];
 
     boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
-boot.initrd.kernelModules = [ "iwlwifi" ];  # Wi-Fi загружается раньше
-boot.kernelModules = [ "kvm-intel" ];
-boot.extraModulePackages = [ ];
+    boot.initrd.kernelModules = [ "iwlwifi" ];
+    boot.kernelModules = [ "kvm-intel" ];
+    boot.extraModulePackages = [ ];
 
     fileSystems."/" =
       { device = "/dev/disk/by-uuid/9df5d26e-4e41-441d-a14c-e5f5f20acd65";
@@ -30,11 +28,3 @@ boot.extraModulePackages = [ ];
       { device = "/dev/disk/by-uuid/71DE-7AC5";
         fsType = "vfat";
         options = [ "fmask=0077" "dmask=0077" ];
-      };
-
-    swapDevices = [ ];
-
-    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-    #hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-  };
-}
